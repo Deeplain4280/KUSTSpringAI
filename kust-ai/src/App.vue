ae53e866f1e11decb34f804db834c14a25bb058c
Deeplain4280 <17584103548@163.com>
2026-08-31 15:39:37 +0800

    前端登陆界面部分

--- kust-ai/src/App.vue
+++ kust-ai/src/App.vue
@@ -1,20 +1,11 @@
 ﻿<template>
-  <div class="h2">
-    <h2>你好，我是vue3的根组件</h2>
-    <h3>你好，我是你的智能助手YY</h3>
-  </div>
-
-  <demo1></demo1>
+  <RouterView></RouterView>
 </template>
 
 <script lang="ts" setup>
-import demo1 from "@/demo/demo1.vue"
+import {RouterView} from "vue-router";
 </script>
 
 <style>
-.h2 {
-  background-color: blue;
-  border-radius: 20px;
-  padding: 20px;
-}
+
 </style>
---
+++ kust-ai/src/views/Login.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+
+  import { ref } from 'vue'
+
+  let account = ref({
+    email: '',
+    code: ''
+  })
+
+  function sendCode() {
+    console.log(account.value)
+  }
+  function login() {
+    console.log(account.value)
+  }
+
+</script>
+
+<template>
+  <div>
+    <p>用户邮箱：<input type="text" v-model = "account.email"></p>
+    <p>验证码：<input type="text" v-model="account.code"></p>
+    <p>
+      <button @click="sendCode()">发送验证码</button>
+      <button @click="login()">登录</button>
+    </p>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
