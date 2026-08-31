35d7d54ae81a39b17be64b88990d96b58803b82a
Deeplain4280 <17584103548@163.com>
2026-08-31 14:00:43 +0800

    学习了ts的两种变量以及v-on事件绑定

--- kust-ai/src/App.vue
+++ kust-ai/src/App.vue
@@ -3,10 +3,12 @@
     <h2>你好，我是vue3的根组件</h2>
     <h3>你好，我是你的智能助手YY</h3>
   </div>
+
+  <demo1></demo1>
 </template>
 
 <script lang="ts" setup>
-
+import demo1 from "@/demo/demo1.vue"
 </script>
 
 <style>
---
+++ kust-ai/src/demo/demo1.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+
+import vue from "@vitejs/plugin-vue";
+
+const username = "小花";
+let age = 30;
+
+import { ref } from "vue";
+let count = ref(1);
+const refusername = ref("马立群")
+
+function addAge() {
+  age = age + 1;
+  console.log("age变量的值正在发生变化", age);
+}
+
+function addCount() {
+  count.value = count.value + 1;
+  console.log("count变量的值正在发生变化", count.value);
+}
+
+</script>
+
+<template>
+  <p>==========插值语法==========</p>
+  <p>username的值{{username}}</p>
+  <p>age的值{{age}}}</p>
+  <p>count的值{{count}}}</p>
+  <p>refusername的值{{refusername}}}</p>
+  <p>==========v-on事件绑定=======</p>
+  <button v-on:click="addAge()"></button>
+  <button @click="addCount()"></button>
+</template>
+
+<style scoped>
+
+</style>
