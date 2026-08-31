7ee90f3e206bf6ad59e240c2a35deb8745a360e6
Deeplain4280 <17584103548@163.com>
2026-08-31 11:33:10 +0800

    将前端 kust-ai 纳入主仓库,解除嵌套 git 引用,前后端统一管理

---
+++ kust-ai/src/App.vue
@@ -0,0 +1,18 @@
+﻿<template>
+  <div class="h2">
+    <h2>你好，我是vue3的根组件</h2>
+    <h3>你好，我是你的智能助手YY</h3>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+.h2 {
+  background-color: blue;
+  border-radius: 20px;
+  padding: 20px;
+}
+</style>
